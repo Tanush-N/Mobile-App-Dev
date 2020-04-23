461924bebcae372c80788e3e66a9f59ac02f46ff
Tanush-N <53893225+Tanush-N@users.noreply.github.com>
2020-04-23 12:03:19 +0530

    Create firebase_member.java

---
+++ login_project/firebase_member.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+
+public class Member {
+
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge(int b) {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Member(){
+
+    }
+}
